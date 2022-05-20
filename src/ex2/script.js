3e243db7deb3fb597d39d8d48974bf1995843e7f
Itay Neuberger <itayneu@gmail.com>
2022-05-20 21:29:35 +0300

    feat: init shuki

---
+++ src/ex2/script.js
@@ -0,0 +1,53 @@
+let pendingTodos = [];
+
+function addTodo() {
+  let currentTodo = document.querySelector("#list-item-input").value;
+
+  if (currentTodo === "") {
+    alert("Add a value to create a new todo");
+  } else if (pendingTodos.includes(currentTodo)) {
+    alert(`The task "${currentTodo}" is already in the list`);
+  } else {
+    let todoListItem = document.createElement("li");
+    // todoListItem.className = "todo";
+    todoListItem.className = "list-item";
+    todoListItem.appendChild(document.createTextNode(currentTodo));
+    document.querySelector("#list").appendChild(todoListItem);
+
+    let deleteSpan = document.createElement("span");
+    deleteSpan.className = "list-item-delete-button";
+    deleteSpan.appendChild(document.createTextNode("\u00D7"));
+    todoListItem.appendChild(deleteSpan);
+
+    // add todo to current todos array
+    pendingTodos.push(currentTodo);
+    document.querySelector("#counter").textContent = pendingTodos.length;
+
+    // alert todo value on li click
+    todoListItem.addEventListener("click", ({ target }) => {
+      if (target.tagName === "LI") {
+        alert(currentTodo);
+      }
+    });
+
+    // delete todo on span click
+    deleteSpan.addEventListener("click", () => {
+      todoListItem.remove();
+
+      // remove todo from current todos array
+      pendingTodos.pop(currentTodo);
+      document.querySelector("#counter").textContent = pendingTodos.length;
+    });
+  }
+
+  // reset current todo input
+  document.querySelector("#list-item-input").value = "";
+}
+
+function clearAllTodos() {
+  document.querySelector("#list").innerHTML = "";
+
+  // empty todos array
+  pendingTodos = [];
+  document.querySelector("#counter").textContent = pendingTodos.length;
+}
