457ee1546422360e8cfccafc0058a344f9055934
Itay Neuberger <itayneu@gmail.com>
2022-05-21 13:07:14 +0300

    feat: separeted to classed

---
+++ src/ex2/DomManager.js
@@ -0,0 +1,42 @@
+class DomManager {
+  constructor() {}
+
+  render(todos) {
+    const ul = document.querySelector("#list");
+    ul.innerHTML = "";
+    todos.forEach((todo) => {
+      const li = document.createElement("li");
+      li.className = "list-item";
+      li.appendChild(document.createTextNode(todo));
+      ul.appendChild(li);
+
+      const span = document.createElement("span");
+      span.className = "list-item-delete-button";
+      span.appendChild(document.createTextNode("\u00D7"));
+      li.appendChild(span);
+
+      // alert todo value on li click
+      li.addEventListener("click", ({ target }) => {
+        if (target.tagName === "LI") {
+          alert(todo);
+        }
+      });
+
+      // delete todo on span click
+      span.addEventListener("click", () => {
+        itemManager.removeTodo(todo);
+        this.render(itemManager.todos);
+      });
+    });
+
+    this.updateCounter(todos);
+  }
+
+  updateCounter(todos) {
+    document.querySelector("#counter").textContent = todos.length;
+  }
+
+  getElement(id) {
+    return document.querySelector(`#${id}`);
+  }
+}
--- src/ex2/main.js
+++ src/ex2/main.js
@@ -1,10 +1,38 @@
 // Implement the `Main` class here
+class Main {
+  constructor() {}
 
+  init() {
+    const submitButton = domManager.getElement("list-item-submit");
+    submitButton.addEventListener("click", () => {
+      let currentTodo = domManager.getElement("list-item-input").value;
+
+      // add todo to current todos array and render
+      itemManager.addTodo(currentTodo);
+      domManager.render(itemManager.todos);
+
+      // reset current todo input
+      domManager.getElement("list-item-input").value = "";
+    });
+
+    const clearAllButton = domManager.getElement("list-item-clear");
+    clearAllButton.addEventListener("click", () => {
+      // empty todos array
+      itemManager.todos.forEach((todo) => {
+        itemManager.removeTodo(todo);
+      });
+
+      domManager.render(itemManager.todos);
+    });
+  }
+}
 
 const main = new Main();
+const itemManager = new ItemManager([]);
+const domManager = new DomManager();
 
 document.addEventListener("DOMContentLoaded", function () {
-    // you should create an `init` method in your class
-    // the method should add the event listener to your "add" button
-    main.init();
+  // you should create an `init` method in your class
+  // the method should add the event listener to your "add" button
+  main.init();
 });
