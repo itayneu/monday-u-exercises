99a89fe6d8bbf741fb45b3249237ab831c8c5eb9
Itay Neuberger <itayneu@gmail.com>
2022-05-26 21:40:32 +0300

    feat: delete svg icon

--- src/ex2/DomManager.js
+++ src/ex2/DomManager.js
@@ -8,26 +8,33 @@ class DomManager {
       li.appendChild(document.createTextNode(todo));
       ul.appendChild(li);
 
-      const span = document.createElement("span");
-      span.className = "list-item-delete-button";
-      span.appendChild(document.createTextNode("\u00D7"));
-      li.appendChild(span);
+      const deleteImg = document.createElement("img");
+      deleteImg.src = "images/delete_icon.svg";
+      deleteImg.className = "list-item-delete-button";
+      li.appendChild(deleteImg);
 
       // alert todo value on li click
-      li.addEventListener("click", ({ target }) => {
-        if (target.tagName === "LI") {
-          alert(todo);
-        }
+      li.addEventListener("click", () => {
+        this.onTodoClick(todo);
       });
 
       // delete todo on span click
-      span.addEventListener("click", () => {
-        itemManager.removeTodo(todo);
-        this.render(itemManager.todos);
+      deleteImg.addEventListener("click", (e) => {
+        this.onTodoDelete(e, todo);
       });
     });
 
     this.updateCounter(todos);
+  }
+
+  onTodoClick(todo) {
+    alert(todo);
+  }
+
+  onTodoDelete(e, todo) {
+    e.stopPropagation();
+    itemManager.removeTodo(todo);
+    this.render(itemManager.todos);
   }
 
   updateCounter(todos) {
