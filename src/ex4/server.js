1aa833581cb366627ea535382c9ba741e23f1aee
Itay Neuberger <itayneu@gmail.com>
2022-06-10 22:29:11 +0300

    feat: init api

--- src/ex4/server.js
+++ src/ex4/server.js
@@ -2,7 +2,28 @@ const express = require("express");
 const port = 8080;
 const app = express();
 
+app.use(bodyParser.json());
 app.use("/static", express.static("public"));
+
+app.use("/todo", api);
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    health: "ok",
+  });
+});
+
+app.post("/error", async (req, res, next) => {
+  try {
+    let error = Error("Error");
+    error.statusCode = 400;
+    throw error;
+  } catch (e) {
+    next(e);
+  }
+});
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log("Server started on port ", port);
--- src/ex4/server/routes/api.js
+++ src/ex4/server/routes/api.js
@@ -1,1 +1,35 @@
 // Define your endpoints here (this is your "controller file")
+const express = require("express");
+
+const api = express.Router();
+
+api.get("/", getAll);
+api.post("/", createTodo);
+api.delete("/:id", deleteTodo);
+
+async function getAll(req, res) {
+  // let data = await jediService.getAll();
+  // if (!data) data = [];
+  // res.status(200).json(data);
+}
+
+async function createTodo(req, res) {
+  console.log("creating a new todo");
+  // await jediService.addJedi(req.body);
+  // res.status(200).json(req.body);
+}
+
+async function deleteTodo(req, res) {
+  let todoId = Number.parseInt(req.params.id);
+  if (isNaN(todoId))
+    try {
+      let error = Error("Wrong parameters");
+      error.statusCode = 400;
+      throw error;
+    } catch (e) {
+      next(e);
+    }
+
+  // const data = await jediService.deleteJedi(todoId);
+  // res.status(200).json(data);
+}
