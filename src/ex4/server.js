0a8713aeb2e13267a0f98c75827bd37fb32f0188
Itay Neuberger <itayneu@gmail.com>
2022-06-10 22:18:44 +0300

    feat: express server init

--- src/ex4/server.js
+++ src/ex4/server.js
@@ -1,1 +1,9 @@
-// Express boilerplate, hosting the `dist` file, connecting to the routes
+const express = require("express");
+const port = 8080;
+const app = express();
+
+app.use("/static", express.static("public"));
+
+app.listen(port, () => {
+  console.log("Server started on port ", port);
+});
