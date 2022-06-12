b94f377c000acbb4090c96c695717920063de2bb
Itay Neuberger <itayneu@gmail.com>
2022-06-12 19:46:40 +0300

    feat: added middlewares

--- src/ex4/server.js
+++ src/ex4/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const logger = require("./server/middleware/logger");
 const todoRouter = require("./server/routes/todoRouter");
 
 const port = 3005;
 const app = express();
 
+app.use(logger);
 app.use(bodyParser.json());
 app.use("/static", express.static("public"));
 
