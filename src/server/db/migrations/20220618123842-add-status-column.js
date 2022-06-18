bc2ceaff9d2ae79f82ffaedf18640af2ce54a9d6
Itay Neuberger <itayneu@gmail.com>
2022-06-18 15:43:59 +0300

    feat: status migration

---
+++ src/server/db/migrations/20220618123842-add-status-column.js
@@ -0,0 +1,16 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn("Items", "status", {
+      type: Sequelize.BOOLEAN,
+      defaultValue: false,
+      allowNull: false,
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeColumn("Items", "status", {
+      type: Sequelize.BOOLEAN,
+    });
+  },
+};
--- src/server/db/models/items.js
+++ src/server/db/models/items.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Items extends Model {
     /**
@@ -13,11 +11,15 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  Items.init({
-    itemName: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Items',
-  });
+  Items.init(
+    {
+      itemName: DataTypes.STRING,
+      status: DataTypes.BOOLEAN,
+    },
+    {
+      sequelize,
+      modelName: "Items",
+    }
+  );
   return Items;
 };
